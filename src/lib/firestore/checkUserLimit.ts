4c955c1934bd9537d5ab5ff66c8adcd2b17c795b
Jason Ballinger <info@knotreels.com>
2025-05-19 09:32:20 +0100

    Initial full project sync to GitHub

--- src/lib/firestore/checkUserLimit.ts
+++
@@ -1,7 +0,0 @@
-import { collection, getCountFromServer } from 'firebase/firestore';
-import { db } from '@/firebase/config';
-
-export const checkUserLimit = async () => {
-  const snapshot = await getCountFromServer(collection(db, 'users'));
-  return snapshot.data().count;
-};
